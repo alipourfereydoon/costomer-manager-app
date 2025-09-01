e5e339bf1fcf4601545256ff75495831a0039d58
ali.pourfereydoon <pourfereydoonali@gmail.com>
2025-08-31 23:07:36 -0700

    creat json-server and use the data in database and if we want to get data from json sever we should use axios then i create service file and then service.js file in it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
+import { getAllContacts, getAllgroups } from "./services/contactService";
 import {
   AddContact,
-  Contact,
+  // Contact,
   Contacts,
   EditContact,
-  Searchcontact,
+  // Searchcontact,
   ViewContact,
   Navbar,
-  sppiner,
+  // sppiner,
 } from "./components";
 
 const App = () => {
   const [loading, setloading] = useState(false);
   const [getcontacts, setContacts] = useState([]);
+  const [getgroups, setGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setloading(true);
+        const { data: contactsData } = await getAllContacts();
+        const { data: groupsData } = await getAllgroups();
+        setContacts(contactsData);
+        setGroups(groupsData);
+        setloading(false);
+      } catch (err) {
+        console.log(err.message);
+        setloading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <Navbar />
@@ -24,6 +44,11 @@ const App = () => {
           path="/contacts"
           element={<Contacts contacts={getcontacts} loading={loading} />}
         />
+        <Route
+          path="/groups"
+          element={<Contacts contacts={getgroups} loading={loading} />}
+        />
+
         <Route path="/contacts/add" element={<AddContact />} />
         <Route path="/contacts/:contactId" element={<ViewContact />} />
         <Route path="/contacts/edit/:contactId" element={<EditContact />} />
--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -1,50 +1,72 @@
 import { RED, CURRENTLINE, CYAN, ORANGE } from "../../helpers/colors";
 
-const Contact = () => {
+const Contact = ({ contact }) => {
   return (
-    <div className="col-md-6">
-      <div style={{ backgroundColor: CURRENTLINE }} className="card my-2">
-        <div className="card-body">
-          <div className="row align-items-center justify-content-around d-flex">
-            <div className="col-md-4 col-sm-4">
-              <img
-                src="https://placehold.co/200"
-                alt=""
-                style={{ border: "1px solid purple " }}
-                className="img-fluid rounded"
-              />
-            </div>
-            <div className="col-md-7 col-sm-7">
-              <ul className="list-group">
-                <li className="list-group-item list-group-item-dark">
-                  name and family-name:{" "}
-                  <span className="fw-bold">ali pourfereydoon</span>
-                </li>
-                <li className="list-group-item list-group-item-dark">
-                  phone number: <span className="fw-bold">009179179170</span>
-                </li>
-                <li className="list-group-item list-group-item-dark">
-                  email address: <span className="fw-bold">ali@yahoo.com</span>
-                </li>
-                <li className="list-group-item list-group-item-dark">
-                  name and family-name:{" "}
-                  <span className="fw-bold">ali pourfereydoon</span>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
-              <button className="btn my-1" style={{ backgroundColor: ORANGE }}>
-                {" "}
-                <i className="fa fa-eye"></i>
-              </button>
-              <button className="btn my-1" style={{ backgroundColor: CYAN }}>
-                {" "}
-                <i className="fa fa-pen"></i>
-              </button>
-              <button className="btn my-1" style={{ backgroundColor: RED }}>
-                {" "}
-                <i className="fa fa-trash"></i>
-              </button>
+    <div className="row">
+      <div
+        style={{ width: "450px", float: "right", padding: "15px" }}
+        className="col-lg-6"
+      >
+        <div style={{ backgroundColor: CURRENTLINE }} className="card my-2">
+          <div className="card-body">
+            <div className="row align-items-center justify-content-around d-flex">
+              <div className="col-md-4 col-sm-4">
+                <img
+                  src="{contact.photo}"
+                  alt={contact.fullname}
+                  style={{ border: "1px solid purple " }}
+                  className="img-fluid rounded"
+                />
+              </div>
+              <div className="col-md-7 col-sm-7">
+                <ul className="list-group">
+                  <li
+                    style={{ listStyleType: "none" }}
+                    className="list-group-item list-group-item-dark"
+                  >
+                    name and family-name:{" "}
+                    <span className="fw-bold">{contact.fullname}</span>
+                  </li>
+                  <li
+                    style={{ listStyleType: "none" }}
+                    className="list-group-item list-group-item-dark"
+                  >
+                    phone number:{" "}
+                    <span className="fw-bold">{contact.phone}</span>
+                  </li>
+                  <li
+                    style={{ listStyleType: "none" }}
+                    className="list-group-item list-group-item-dark"
+                  >
+                    email address:{" "}
+                    <span className="fw-bold">{contact.email}</span>
+                  </li>
+                  <li
+                    style={{ listStyleType: "none" }}
+                    className="list-group-item list-group-item-dark"
+                  >
+                    name and family-name:{" "}
+                    <span className="fw-bold">{contact.fullname}</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
+                <button
+                  className="btn my-1"
+                  style={{ backgroundColor: ORANGE }}
+                >
+                  {" "}
+                  <i className="fa fa-eye"></i>
+                </button>
+                <button className="btn my-1" style={{ backgroundColor: CYAN }}>
+                  {" "}
+                  <i className="fa fa-pen"></i>
+                </button>
+                <button className="btn my-1" style={{ backgroundColor: RED }}>
+                  {" "}
+                  <i className="fa fa-trash"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
